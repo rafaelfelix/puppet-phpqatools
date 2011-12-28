4e4abacdae8abd652a0be0162aaebfe8bc92c60d
Rafael Felix Correa <rafael.felix@rf4solucoes.com.br>
2011-12-27 22:49:01 -0200

    updating hash version of pear provider

--- modules/puppet-pear/lib/puppet/provider/package/pear.rb
+++ modules/puppet-pear/lib/puppet/provider/package/pear.rb
@@ -1,5 +1,5 @@
 # Puppet PHP PEAR Package support
-# Taken from https://raw.github.com/gist/1524864/1a468cb768bee75f8b3727939896806c997b59db/pear.rb
+# Taken from https://raw.github.com/gist/1524864/fdd8fd047aa702587c0b7cce7a77d55dbd781f01/pear.rb
 
 require 'puppet/provider/package'
 
