585daf54b61211bfee5266786686d861e37f8632
Rafael Felix Correa <rafael.correa@abril.com.br>
2011-12-29 14:35:29 -0200

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: phpqatools
+#
+# This module manages phpqatools
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class phpqatools {
+
+
+}
