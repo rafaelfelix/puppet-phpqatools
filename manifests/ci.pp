66111c4e9fa28e8781c602c1481cc48dc2fa2451
Rafael Felix Correa <rafael.felix@rf4solucoes.com.br>
2011-12-29 00:56:21 -0200

    grouping code using classes

--- manifests/ci.pp
+++ manifests/ci.pp
@@ -1,138 +1,184 @@
-yumrepo { "IUS":
-	baseurl => "http://dl.iuscommunity.org/pub/ius/stable/Redhat/5/$architecture",
-	descr => "IUS Community repository",
-	enabled => 1,
-	gpgcheck => 0
-}
-
 ## PHP Quality Tools
-
-# PHP
-package { "php53u-pear":
-	ensure => present,
-	require => Yumrepo["IUS"]
-}
-
-# PEAR Package
-package { "PEAR": 
-	provider => "pear",
-	ensure => latest,
-	require => Package["php53u-pear"]
-}
-
-# PHPUnit
-package { "PHPUnit":
-	provider => "pear",
-	source => "pear.phpunit.de/PHPUnit",
-	ensure => latest,
-	require => Package["PEAR"]
-}
-
-# Pdepend
-package { "PHP_Depend":
-	provider => "pear",
-	source => "pear.pdepend.org/PHP_Depend-beta",
-	ensure => latest,
-	require => Package["PEAR"]
-}
-
-# PHPMD
-package { "PHP_PMD":
-	provider => "pear",
-	source => "pear.phpmd.org/PHP_PMD",
-	ensure => latest,
-	require => Package["PHP_Depend"]
-}
-
-# PHP CPD
-package { "Base":
-	provider => "pear",
-	source => "components.ez.no/Base",
-	ensure => latest,
-	require => Package["PEAR"]
-}
-
-package { "ConsoleTools":
-	provider => "pear",
-	source => "components.ez.no/ConsoleTools",
-	ensure => latest,
-	require => Package["Base"]
-}
-
-package { "File_Iterator":
-	provider => "pear",
-	source => "pear.phpunit.de/File_Iterator",
-	ensure => latest,
-	require => Package["PEAR"]
-}
-
-package { "PHPCPD":
-	provider => "pear",
-	source => "pear.phpunit.de/phpcpd",
-	ensure => latest,
-	require => Package["Base"]
-}
-
-# PHPLOC
-package { "PHPLOC":
-	provider => "pear",
-	source => "pear.phpunit.de/phploc",
-	ensure => latest,
-	require => Package["Base"]
-}
-
-# PHPDCD
-package { "PHPDCD":
-	provider => "pear",
-	source => "pear.phpunit.de/phpdcd",
-	ensure => latest,
-	require => Package["Base"]
-}
-
-# PHP_CodeSniffer
-package { "PHP_CodeSniffer":
-	provider => "pear",
-	source => "pear.php.net/PHP_CodeSniffer",
-	ensure => latest,
-	require => Package["PEAR"]
-}
-
-# Bytekit
-package { "bytekit":
-	provider => "pear",
-	source => "pear.phpunit.de/bytekit",
-	ensure => latest,
-	require => Package["File_Iterator"]
-}
-
-# Phing
-package { "Phing":
-	provider => "pear",
-	source => "pear.phing.info/phing",
-	ensure => latest,
-	require => Package["PEAR"]
+class php::qatools {
+    yumrepo { "IUS":
+    	baseurl => "http://dl.iuscommunity.org/pub/ius/stable/Redhat/5/$architecture",
+    	descr => "IUS Community repository",
+    	enabled => 1,
+    	gpgcheck => 0
+    }
+    
+    # PHP
+    package { "php53u-pear":
+    	ensure => present,
+    	require => Yumrepo["IUS"]
+    }
+    
+    # PEAR Package
+    package { "PEAR": 
+    	provider => "pear",
+    	ensure => latest,
+    	require => Package["php53u-pear"]
+    }
+    
+    # PHPUnit
+    package { "PHPUnit":
+    	provider => "pear",
+    	source => "pear.phpunit.de/PHPUnit",
+    	ensure => latest,
+    	require => Package["PEAR"]
+    }
+    
+    # Pdepend
+    package { "PHP_Depend":
+    	provider => "pear",
+    	source => "pear.pdepend.org/PHP_Depend-beta",
+    	ensure => latest,
+    	require => Package["PEAR"]
+    }
+    
+    # PHPMD
+    package { "PHP_PMD":
+    	provider => "pear",
+    	source => "pear.phpmd.org/PHP_PMD",
+    	ensure => latest,
+    	require => Package["PHP_Depend"]
+    }
+    
+    # PHP CPD
+    package { "Base":
+    	provider => "pear",
+    	source => "components.ez.no/Base",
+    	ensure => latest,
+    	require => Package["PEAR"]
+    }
+    
+    package { "ConsoleTools":
+    	provider => "pear",
+    	source => "components.ez.no/ConsoleTools",
+    	ensure => latest,
+    	require => Package["Base"]
+    }
+    
+    package { "File_Iterator":
+    	provider => "pear",
+    	source => "pear.phpunit.de/File_Iterator",
+    	ensure => latest,
+    	require => Package["PEAR"]
+    }
+    
+    package { "PHPCPD":
+    	provider => "pear",
+    	source => "pear.phpunit.de/phpcpd",
+    	ensure => latest,
+    	require => Package["Base"]
+    }
+    
+    # PHPLOC
+    package { "PHPLOC":
+    	provider => "pear",
+    	source => "pear.phpunit.de/phploc",
+    	ensure => latest,
+    	require => Package["Base"]
+    }
+    
+    # PHPDCD
+    package { "PHPDCD":
+    	provider => "pear",
+    	source => "pear.phpunit.de/phpdcd",
+    	ensure => latest,
+    	require => Package["Base"]
+    }
+    
+    # PHP_CodeSniffer
+    package { "PHP_CodeSniffer":
+    	provider => "pear",
+    	source => "pear.php.net/PHP_CodeSniffer",
+    	ensure => latest,
+    	require => Package["PEAR"]
+    }
+    
+    # Bytekit
+    package { "bytekit":
+    	provider => "pear",
+    	source => "pear.phpunit.de/bytekit",
+    	ensure => latest,
+    	require => Package["File_Iterator"]
+    }
+    
+    # Phing
+    package { "Phing":
+    	provider => "pear",
+    	source => "pear.phing.info/phing",
+    	ensure => latest,
+    	require => Package["PEAR"]
+    }
 }
 
 ## Jenkins CI Server
+class php::jenkins-ci inherits jenkins {
 
-package { "java-1.6.0-openjdk":
-	ensure => installed
+    package { "java-1.6.0-openjdk":
+    	ensure => installed
+    }
+    
+    # Jenkins plugins
+    install-jenkins-plugin {
+    	"git-plugin" :
+    		name => "git";
+    }
+    
+    install-jenkins-plugin {
+    	"checkstyle" :
+    		name => "checkstyle";
+    }
+    
+    install-jenkins-plugin {
+    	"cobertura" :
+    		name => "cobertura";
+    }
+    
+    install-jenkins-plugin {
+        "cloverphp" :
+            name => "cloverphp";
+    }
+    
+    install-jenkins-plugin {
+        "dry" :
+            name => "dry";
+    }
+    
+    install-jenkins-plugin {
+        "htmlpublisher" :
+            name => "htmlpublisher";
+    }
+    
+    install-jenkins-plugin {
+        "jdepend" :
+            name => "jdepend";
+    }
+    
+    install-jenkins-plugin {
+        "plot" :
+            name => "plot";
+    }
+    
+    install-jenkins-plugin {
+        "pmd" :
+            name => "pmd";
+    }
+    
+    install-jenkins-plugin {
+        "violations" :
+            name => "violations";
+    }
+    
+    install-jenkins-plugin {
+        "xunit" :
+            name => "xunit";
+    }
 }
 
-include jenkins
+include php::qatools
+include php::jenkins-ci
 
-# Jenkins plugins
-install-jenkins-plugin {
-	"git-plugin" :
-		name => "git";
-}
-
-install-jenkins-plugin {
-	"checkstyle" :
-		name => "checkstyle";
-}
-
-install-jenkins-plugin {
-	"cobertura" :
-		name => "cobertura";
-}
+Class["php::qatools"] -> Class["php::jenkins-ci"]
