9491e49efe9b61aa672c3dd65cb9b57296fbd419
Rafael Felix Correa <rafael.felix@gmail.com>
2012-01-20 18:58:29 -0200

    adjusted line indentation + included pear lib

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,97 +12,99 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class phpqatools {
+	include pear
+
 	yumrepo { "IUS":
 		baseurl => "http://dl.iuscommunity.org/pub/ius/stable/Redhat/5/$architecture",
 		descr => "IUS Community repository",
 		enabled => 1,
 		gpgcheck => 0
 	}
-	    
+
 	class { "pear":
 		package => "php53u-pear",
 		require => Yumrepo["IUS"],
 	}
-	
+
 	# PEAR Package
-    pear::package { "PEAR": }
-    
-    # PHPUnit
+	pear::package { "PEAR": }
+
+	# PHPUnit
 	pear::package { "PHPUnit":
 		version => "latest",
 		repository => "pear.phpunit.de",
 		require => Pear::Package["PEAR"],
 	}
-    
-    # Pdepend
-    pear::package { "PHP_Depend":
+
+	# Pdepend
+	pear::package { "PHP_Depend":
 		version => "beta",
 		repository => "pear.pdepend.org",
 		require => Pear::Package["PEAR"],
 	}
-    
-    # PHPMD
-    pear::package { "PHP_PMD":
+
+	# PHPMD
+	pear::package { "PHP_PMD":
 		version => "latest",
 		repository => "pear.phpmd.org",
 		require => Pear::Package["PHP_Depend"],
 	}
-    
-    # PHP CPD
-    pear::package { "Base":
+
+	# PHP CPD
+	pear::package { "Base":
 		version => "latest",
 		repository => "components.ez.no",
 		require => Pear::Package["PEAR"],
 	}
 
-    pear::package { "ConsoleTools":
+	pear::package { "ConsoleTools":
 		version => "latest",
 		repository => "components.ez.no",
 		require => Pear::Package["Base"],
 	}
 
-    pear::package { "File_Iterator":
+	pear::package { "File_Iterator":
 		version => "latest",
 		repository => "pear.phpunit.de",
 		require => Pear::Package["PEAR"],
 	}
 
-    pear::package { "phpcpd":
+	pear::package { "phpcpd":
 		version => "latest",
 		repository => "pear.phpunit.de",
 		require => Pear::Package["Base"],
 	}
 
-    # PHPLOC
-    pear::package { "phploc":
+	# PHPLOC
+	pear::package { "phploc":
 		version => "latest",
 		repository => "pear.phpunit.de",
 		require => Pear::Package["Base"],
 	}
 
-    # PHPDCD
-    pear::package { "phpdcd":
+	# PHPDCD
+	pear::package { "phpdcd":
 		version => "latest",
 		repository => "pear.phpunit.de",
 		require => Pear::Package["Base"],
 	}
-    
-    # PHP_CodeSniffer
-    pear::package { "PHP_CodeSniffer":
+
+	# PHP_CodeSniffer
+	pear::package { "PHP_CodeSniffer":
 		version => "latest",
 		repository => "pear.php.net",
 		require => Pear::Package["PEAR"],
 	}
-    
-    # Bytekit
-    pear::package { "bytekit":
+
+	# Bytekit
+	pear::package { "bytekit":
 		version => "latest",
 		repository => "pear.phpunit.de",
 		require => Pear::Package["File_Iterator"],
 	}
-    
-    # Phing
-    pear::package { "Phing":
+
+	# Phing
+	pear::package { "Phing":
 		version => "latest",
 		repository => "pear.phing.info",
 		require => Pear::Package["PEAR"],
