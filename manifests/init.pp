b5cb1a6c04dab4abca4b66a4a1598c287042da6b
Dennis Philpot <d.philpot@reuter.de>
2014-06-21 15:55:52 +0200

    puppet-lint throws no error now

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,120 +13,120 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class phpqatools (
-	$standard_hash = {},
-	$default_standard = undef,
+  $standard_hash = {},
+  $default_standard = undef,
 ) {
-	include pear
+  include pear
 
-	# PEAR Package
-	pear::package { "PEAR": }
+  # PEAR Package
+  pear::package { 'PEAR': }
 
-	# PHPUnit
-	pear::package { "PHPUnit":
-		version => "latest",
-		repository => "pear.phpunit.de",
-		require => Pear::Package["PEAR"],
-	}
+  # PHPUnit
+  pear::package { 'PHPUnit':
+    version    => 'latest',
+    repository => 'pear.phpunit.de',
+    require    => Pear::Package['PEAR'],
+  }
 
-	# Pdepend
-	pear::package { "PHP_Depend":
-		version => "latest",
-		repository => "pear.pdepend.org",
-		require => Pear::Package["PEAR"],
-	}
+  # Pdepend
+  pear::package { 'PHP_Depend':
+    version    => 'latest',
+    repository => 'pear.pdepend.org',
+    require    => Pear::Package['PEAR'],
+  }
 
-	# PHPMD
-	pear::package { "PHP_PMD":
-		version => "latest",
-		repository => "pear.phpmd.org",
-		require => Pear::Package["PHP_Depend"],
-	}
+  # PHPMD
+  pear::package { 'PHP_PMD':
+    version    => 'latest',
+    repository => 'pear.phpmd.org',
+    require    => Pear::Package['PHP_Depend'],
+  }
 
-	# PHP CPD
-	pear::package { "Base":
-		version => "latest",
-		repository => "components.ez.no",
-		require => Pear::Package["PEAR"],
-	}
+  # PHP CPD
+  pear::package { 'Base':
+    version    => 'latest',
+    repository => 'components.ez.no',
+    require    => Pear::Package['PEAR'],
+  }
 
-	pear::package { "ConsoleTools":
-		version => "latest",
-		repository => "components.ez.no",
-		require => Pear::Package["Base"],
-	}
+  pear::package { 'ConsoleTools':
+    version    => 'latest',
+    repository => 'components.ez.no',
+    require    => Pear::Package['Base'],
+  }
 
-	pear::package { "File_Iterator":
-		version => "latest",
-		repository => "pear.phpunit.de",
-		require => Pear::Package["PEAR"],
-	}
+  pear::package { 'File_Iterator':
+    version    => 'latest',
+    repository => 'pear.phpunit.de',
+    require    => Pear::Package['PEAR'],
+  }
 
-	# First install finder dependency
-	pear::package { "Finder":
-		version		=> "latest",
-		repository	=> "pear.symfony.com",
-		require => Pear::Package["Base"],
-	}
+  # First install finder dependency
+  pear::package { 'Finder':
+    version    => 'latest',
+    repository => 'pear.symfony.com',
+    require    => Pear::Package['Base'],
+  }
 
-	pear::package { "phpcpd":
-		version => "latest",
-		repository => "pear.phpunit.de",
-		require => Pear::Package["Finder"],
-	}
+  pear::package { 'phpcpd':
+    version    => 'latest',
+    repository => 'pear.phpunit.de',
+    require    => Pear::Package['Finder'],
+  }
 
-	# PHPLOC
-	pear::package { "phploc":
-		version => "latest",
-		repository => "pear.phpunit.de",
-		require => Pear::Package["Base"],
-	}
+  # PHPLOC
+  pear::package { 'phploc':
+    version    => 'latest',
+    repository => 'pear.phpunit.de',
+    require    => Pear::Package['Base'],
+  }
 
-	# PHPDCD
-	pear::package { "phpdcd":
-		version => "latest",
-		repository => "pear.phpunit.de",
-		require => Pear::Package["Base"],
-	}
+  # PHPDCD
+  pear::package { 'phpdcd':
+    version    => 'latest',
+    repository => 'pear.phpunit.de',
+    require    => Pear::Package['Base'],
+  }
 
-	# PHP_CodeSniffer
-	pear::package { "PHP_CodeSniffer":
-		version => "latest",
-		repository => "pear.php.net",
-		require => Pear::Package["PEAR"],
-	}
+  # PHP_CodeSniffer
+  pear::package { 'PHP_CodeSniffer':
+    version    => 'latest',
+    repository => 'pear.php.net',
+    require    => Pear::Package['PEAR'],
+  }
 
-	# Bytekit
-	pear::package { "bytekit":
-		version => "latest",
-		repository => "pear.phpunit.de",
-		require => Pear::Package["File_Iterator"],
-	}
+  # Bytekit
+  pear::package { 'bytekit':
+    version    => 'latest',
+    repository => 'pear.phpunit.de',
+    require    => Pear::Package['File_Iterator'],
+  }
 
-	# Phing
-	pear::package { "phing":
-		version => "latest",
-		repository => "pear.phing.info",
-		require => Pear::Package["PEAR"],
-	}
+  # Phing
+  pear::package { 'phing':
+    version    => 'latest',
+    repository => 'pear.phing.info',
+    require    => Pear::Package['PEAR'],
+  }
 
-	pear::package { "phpDox":
-		version => 'latest',
-		repository => "pear.netpirates.net"
-	}
+  pear::package { 'phpDox':
+    version    => 'latest',
+    repository => 'pear.netpirates.net'
+  }
 
-	pear::package { "PhpDocumentor":
-		version => '1.4.4',
-		repository => "pear.php.net",
-	}
+  pear::package { 'PhpDocumentor':
+    version    => '1.4.4',
+    repository => 'pear.php.net',
+  }
 
-	# install additional coding standards
-	create_resources('vcsrepo', $standard_hash)
-   
-	if $default_standard != undef {
-	  exec {"phpcs --config-set default_standard ${default_standard}":
-		cwd => '/tmp',
-		path => ['/usr/bin'],
-		creates => '/usr/share/pear/data/PHP_CodeSniffer/CodeSniffer.conf'
-	  }
-	}
+  # install additional coding standards
+  create_resources('vcsrepo', $standard_hash)
+
+  if $default_standard != undef {
+    exec {"phpcs --config-set default_standard ${default_standard}":
+      cwd     => '/tmp',
+      path    => ['/usr/bin'],
+      creates => '/usr/share/pear/data/PHP_CodeSniffer/CodeSniffer.conf'
+    }
+  }
 }
